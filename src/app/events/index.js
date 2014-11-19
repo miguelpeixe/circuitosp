83389f6a1926f21170beba3b831aa6f9454015c9
Miguel Peixe <miguel@cardume.art.br>
2014-11-19 04:06:56 -0200

    fix #35

--- src/app/events/index.js
+++ src/app/events/index.js
@@ -39,6 +39,50 @@ angular.module('mci.events', [
 	}
 ])
 
+.directive('occurrenceSlider', [
+	'$timeout',
+	'$interval',
+	function($timeout, $interval) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+
+				scope.$watch(function() {
+					return scope.filteredOccurs.length;
+				}, function() {
+					scope.current = 0;
+				});
+
+				scope.current = 0;
+
+				scope.setOccur = function(id) {
+					scope.current = id;
+					$timeout(function() {
+						$(window).trigger('resize');
+					}, 500);
+				};
+
+				scope.nextOccur = function() {
+					if(scope.filteredOccurs[scope.current+1]) {
+						scope.setOccur(scope.current+1);
+					} else {
+						scope.setOccur(0);
+					}
+				}
+
+				scope.prevOccur = function() {
+					if(scope.filteredOccurs[scope.current-1]) {
+						scope.setOccur(scope.current-1);
+					} else {
+						scope.setOccur(scope.filteredOccurs.length-1);
+					}
+				}
+
+			}
+		}
+	}
+])
+
 .factory('EventService', require('./EventService'))
 .factory('RelativeDateService', require('./RelativeDateService'))
 .controller('EventController', require('./EventController'))
